c2b07bfdb9f5d0c69b841b52105f3faecc8a3919
Eden Toledano <edentoledano@gmail.com>
2017-02-01 23:24:47 +0200

    Added validation to form in contact page

--- public/controllers.js
+++ public/controllers.js
@@ -1,4 +1,4 @@
-directTV.controller('mainController', ['$scope', '$state', '$http', function($scope, $state, $http) {
+directTV.controller('mainController', ['$scope', '$state', '$http', 'validator', function($scope, $state, $http, validator) {
 
     $scope.currentTab = {
         about: false,
@@ -12,7 +12,19 @@ directTV.controller('mainController', ['$scope', '$state', '$http', function($sc
         name: '',
         email: '',
         message: ''
-    }
+    };
+
+    var isValid = {
+        name: false,
+        email: false,
+        message: false
+    };
+
+    var error = {
+        BAD_EMAIL: 'אנא מלא כתובת מייל תקינה על מנת שנוכל לחזור אלייך',
+        BAD_FORM: 'אנא בדוק תקינות הטופס'
+    };
+
 
     var initView = function() {
         $scope.$on('$stateChangeSuccess',
@@ -37,34 +49,50 @@ directTV.controller('mainController', ['$scope', '$state', '$http', function($sc
 
     $scope.submitForm = function() {
         console.log("submitting... need to do some validation", $scope.formData);
-        $http({
-            url: "https://formspree.io/sales@dtv.org.il",
-            data: $.param({
-                email: $scope.formData.email,
-                name: $scope.formData.name,
-                message: $scope.formData.message
-            }),
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-        }).then(
-            function success(response) {
-                console.log("response", response);
-                alert("תודה שפנית אלינו, ניצור איתך קשר בהקדם");
-                $scope.formData = {
-                    name: '',
-                    email: '',
-                    message: ''
-                }
+        isValid = {
+            name: validator.isFilled($scope.formData.name),
+            email: validator.isEmailValid($scope.formData.email),
+            message: validator.isFilled($scope.formData.message)
+        }
+        console.log("Validation: ", isValid);
+        if (isValid.name && isValid.email && isValid.message) {
+            // if form valid, make request
+            $http({
+                url: "https://formspree.io/sales@dtv.org.il",
+                data: $.param({
+                    email: $scope.formData.email,
+                    name: $scope.formData.name,
+                    message: $scope.formData.message
+                }),
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+            }).then(
+                function success(response) {
+                    console.log("response", response);
+                    alert("תודה שפנית אלינו, ניצור איתך קשר בהקדם");
+                    $scope.formData = {
+                        name: '',
+                        email: '',
+                        message: ''
+                    }
 
-            },
-            function error(response) {
-                console.log("response", response);
-                alert("אנא בדוק תקינות הטופס")
+                },
+                function error(response) {
+                    console.log("response", response);
+                    alert(error.BAD_FORM);
+                }
+            );
+        } else {
+            if (!isValid.email && isValid.name && isValid.message) {
+                alert(error.BAD_EMAIL);
+            } else {
+                alert(error.BAD_FORM);
             }
-        );
+        }
+
     }
     initView();
 
